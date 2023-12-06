6a874959adf88301b3c5ed9f4230c7c5b40874cc
Bakhtiyar Ismayil <bakhtiyar.ismayil@gmail.com>
2023-12-06 20:21:16 +0400

    Wed Dec  6 08:21:16 PM +04 2023

---
+++ expiration_date.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+a=$(sudo chage -l test)
+b=$(echo "$a" | grep 'Password expires')
+c=$(echo "$b" | awk -F': ' '{print $2}')
+d=$(date -d "$c" +%Y%m%d)
+e=$(date +%Y%m%d)
+f=$(date -d "$e" +%s)
+g=$(date -d "$d" +%s)
+h=$((g - f))
+l=$((h / (60*60*24)))
+echo $l
+
