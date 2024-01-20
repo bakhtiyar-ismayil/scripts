728f62979c6a40f112615dccb1b6f041e538aa4a
Bakhtiyar Ismayil <bakhtiyar.ismayil@gmail.com>
2024-01-20 12:28:26 +0400

    Sat Jan 20 12:28:26 PM +04 2024

--- snmp_v3_check.sh
+++ snmp_v3_check.sh
@@ -11,3 +11,7 @@ SNMP_PRIV_PASSPHRASE='snmp_check'
 # Perform SNMPv3 walk
 snmpwalk -v 3 -u "$SNMP_USER" -a "$SNMP_AUTH_PROTOCOL" -A "$SNMP_AUTH_PASSPHRASE" -x "$SNMP_PRIV_PROTOCOL" -X "$SNMP_PRIV_PASSPHRASE" "$TARGET_IP"
 
+
+#snmpwalk -v 2c -c {$SNMP_COMMUNITY} NETWORK 172.13.0.2
+#snmpwalk -v 2c -c NETWORK 172.13.0.2 system
+#snmpwalk -v 2c -c public 172.13.0.2
